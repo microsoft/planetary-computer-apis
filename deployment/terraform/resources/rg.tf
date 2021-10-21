b6f303578114cd28dd21974a5045f995697b1873
James Santucci <james.santucci@gmail.com>
2021-10-21 14:32:02 -0600

    add back mqe resource group
    why was this deleted
    
    am i being trolled

--- deployment/terraform/resources/rg.tf
+++ deployment/terraform/resources/rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "pqe" {
   name     = "${local.prefix}_rg"
   location = var.region
 }
+
+resource "azurerm_resource_group" "mqe" {
+  name     = "${local.prefix}_rg"
+  location = var.region
+}
