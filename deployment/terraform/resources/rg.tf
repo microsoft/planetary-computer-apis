69cf7fe3ea4d8002422b63840f70fb39614d85e4
James Santucci <james.santucci@gmail.com>
2021-10-25 14:26:37 -0600

    re-remove the mqe resource group
    because it was deliberately missing, not accidentally

--- deployment/terraform/resources/rg.tf
+++ deployment/terraform/resources/rg.tf
@@ -2,8 +2,3 @@ resource "azurerm_resource_group" "pqe" {
   name     = "${local.prefix}_rg"
   location = var.region
 }
-
-resource "azurerm_resource_group" "mqe" {
-  name     = "${local.prefix}_rg"
-  location = var.region
-}
