8840e3ee539f9e39a2361b189624c4e77545b39e
Rob Emanuele <rdemanuele@gmail.com>
2021-10-28 17:33:12 -0400

    Remove testdata, move loadtestdata to tests

--- stac/tests/loadtestdata.py
+++ stac/tests/loadtestdata.py
@@ -4,13 +4,15 @@ from pathlib import Path
 import orjson
 from pypgstac.load import DB, load_iterator, loadopt, tables
 
+from tests.util.test_data import TestData
+
 
 async def load_test_data() -> None:
     async with DB() as conn:
-        with open("/opt/testdata/collection.json", "rb") as f:
+        with open(TestData.get_path("naip/collection.json"), "rb") as f:
             c = orjson.loads(f.read())
             await load_iterator([c], tables.collections, conn, loadopt.upsert)
-        pathlist = Path("/opt/testdata/items/").glob("*.json")
+        pathlist = Path(TestData.get_path("naip/items")).glob("*.json")
         for path in pathlist:
             with open(str(path), "rb") as f:
                 i = orjson.loads(f.read())
