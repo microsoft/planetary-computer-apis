1ef3f3ecddcd97f7ffabc2be7972a3a2fcacd3d2
James Sanntucci <james.santucci@gmail.com>
2021-11-02 09:58:24 -0600

    include extra metadata with exception

--- pctiler/pctiler/middleware.py
+++ pctiler/pctiler/middleware.py
@@ -143,7 +143,11 @@ async def handle_exceptions(request: Request, call_next: Callable[[Request], Awa
     except Exception as e:
         logger.exception("Exception when handling request", extra={
             "custom_dimensions": {
-                "stackTrace": f"{e}"
+                "stackTrace": f"{e}",
+                HTTP_URL: str(request.url),
+                HTTP_METHOD: str(request.method),
+                HTTP_PATH: request_to_path(request),
+                "service": "tiler"
             }
         })
         raise
