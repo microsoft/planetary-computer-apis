ae623922781eb8b35c6845fb8cfcbbd6af27240c
Matt McFarland <mmcfarland@microsoft.com>
2022-01-19 21:22:18 -0500

    Keep /items endpoint at default limit of 10

--- pcstac/pcstac/api.py
+++ pcstac/pcstac/api.py
@@ -1,11 +1,8 @@
 from typing import Any, Dict, Optional
 
 from stac_fastapi.api.app import StacApi
-from stac_fastapi.api.models import create_request_model
-from stac_pydantic import ItemCollection
 
 from pccommon.openapi import fixup_schema
-from pcstac.search import PCItemCollectionUri
 
 STAC_API_OPENAPI_TAG = "STAC API v1.0.0-beta.2"
 
@@ -26,30 +23,3 @@ class PCStacApi(StacApi):
         schema = super().customize_openapi()
         assert schema is not None
         return fixup_schema(self.app.root_path, schema, tag=STAC_API_OPENAPI_TAG)
-
-    # TODO: Allow overriding base_model for ItemCollectionUri upstream in stac-fastapi
-    def register_get_item_collection(self) -> None:
-        """Register get item collection endpoint (GET /collection/{collection_id}/items).
-        Returns:
-            None
-        """
-        get_pagination_model = self.get_extension(self.pagination_extension).GET
-        request_model = create_request_model(
-            "ItemCollectionURI",
-            base_model=PCItemCollectionUri,
-            mixins=[get_pagination_model],
-        )
-        self.router.add_api_route(
-            name="Get ItemCollection",
-            path="/collections/{collection_id}/items",
-            response_model=ItemCollection
-            if self.settings.enable_response_models
-            else None,
-            response_class=self.response_class,
-            response_model_exclude_unset=True,
-            response_model_exclude_none=True,
-            methods=["GET"],
-            endpoint=self._create_endpoint(
-                self.client.item_collection, request_model, self.response_class
-            ),
-        )
