524bba367aba5ac8b18045c810f8c3f23a6dc9b1
Nathan Zimmerman <npzimmerman@gmail.com>
2022-02-08 08:46:05 -0600

    Use cache and guard against errors

--- pcstac/pcstac/cache.py
+++ pcstac/pcstac/cache.py
@@ -3,3 +3,7 @@ from cachetools import TTLCache
 # A TTL cache, only used for the (large) '/collections' endpoint
 # TTL set to 600 seconds == 10 minutes
 collections_endpoint_cache: TTLCache = TTLCache(maxsize=1, ttl=600)
+
+# A TTL cache, only used for computed all-collection queryables
+# TTL set to 21600 seconds == 6 hours
+queryables_endpoint_cache: TTLCache = TTLCache(maxsize=1, ttl=21600)
--- pcstac/pcstac/filter.py
+++ pcstac/pcstac/filter.py
@@ -1,15 +1,17 @@
 import asyncio
+import json
 from typing import Any, Dict, List, Optional, Set
 
 import requests
 from fastapi import HTTPException, Request
 from stac_fastapi.types.core import AsyncBaseFiltersClient
 
+from pcstac.cache import queryables_endpoint_cache
+
 
 class MSPCFiltersClient(AsyncBaseFiltersClient):
     """Defines a pattern for implementing the STAC filter extension."""
 
-    queryable_intersection = None
     queryable_url_template = (
         "https://planetarycomputer.microsoft.com/stac/{cid}/queryables.json"
     )
@@ -32,27 +34,34 @@ class MSPCFiltersClient(AsyncBaseFiltersClient):
             )
         collection_ids = [collection["id"] for collection in collections]
         all_queryables = await asyncio.gather(
-            *[self.get_queryables(cid) for cid in collection_ids]
+            *[self.get_queryables(cid) for cid in collection_ids],
+            return_exceptions=True
         )
+        all_queryables = [
+            queryable
+            for queryable in all_queryables
+            if not isinstance(queryable, Exception)
+        ]
         all_properties: List[dict] = [
             queryable["properties"] for queryable in all_queryables
         ]
         all_property_keys: List[Set[str]] = list(
             map(lambda x: set(x.keys()), all_properties)
         )
-        property_name_intersection: List[str] = list(
-            set.intersection(*all_property_keys)
-        )
         intersecting_props = {}
-        for name in property_name_intersection:
-            for idx, properties in enumerate(all_properties):
-                if idx == 0:
-                    maybe_match = properties[name]
-                else:
-                    if properties[name] != maybe_match:
-                        break
-            if maybe_match is not None:
-                intersecting_props[name] = maybe_match
+        if len(all_property_keys) > 0:
+            property_name_intersection: List[str] = list(
+                set.intersection(*all_property_keys)
+            )
+            for name in property_name_intersection:
+                for idx, properties in enumerate(all_properties):
+                    if idx == 0:
+                        maybe_match = properties[name]
+                    else:
+                        if properties[name] != maybe_match:
+                            break
+                if maybe_match is not None:
+                    intersecting_props[name] = maybe_match
 
         intersection_schema = {
             "$schema": "http://json-schema.org/draft-07/schema#",
@@ -61,7 +70,6 @@ class MSPCFiltersClient(AsyncBaseFiltersClient):
             "title": "",
             "properties": intersecting_props,
         }
-        self.queryable_intersection = intersection_schema
         return intersection_schema
 
     async def get_queryables(
@@ -75,16 +83,20 @@ class MSPCFiltersClient(AsyncBaseFiltersClient):
         https://github.com/radiantearth/stac-api-spec/tree/master/fragments/filter#queryables
         """
         if not collection_id:
-            if self.queryable_intersection:
-                queryable_resp = self.queryable_intersection
-            else:
+            try:
+                queryable_resp = queryables_endpoint_cache["/collections"]
+            except KeyError:
                 request = kwargs["request"]
                 if isinstance(request, Request):
                     queryable_resp = await self.get_queryable_intersection(request)
+                    queryables_endpoint_cache["/queryables"] = queryable_resp
         else:
             r = requests.get(self.queryable_url_template.format(cid=collection_id))
             if r.status_code == 404:
                 raise HTTPException(status_code=404)
             elif r.status_code == 200:
-                queryable_resp = r.json()
+                try:
+                    queryable_resp = r.json()
+                except json.decoder.JSONDecodeError:
+                    raise HTTPException(status_code=404)
         return queryable_resp
