7df82cf3f306307f7abfa1ef3a887a7f3c033ba4
Rob Emanuele <rdemanuele@gmail.com>
2022-02-09 21:39:12 -0500

    Add mosiac/info endpoint

--- pctiler/pctiler/endpoints/pg_mosaic.py
+++ pctiler/pctiler/endpoints/pg_mosaic.py
@@ -1,6 +1,9 @@
 from dataclasses import dataclass
 
-from fastapi import Query
+from fastapi import Query, Request
+from fastapi.responses import ORJSONResponse
+from pccommon.config import get_collection_config
+from pccommon.config.collections import MosaicInfo
 from titiler.core import dependencies
 from titiler.pgstac.factory import MosaicTilerFactory
 
@@ -21,3 +24,22 @@ pgstac_mosaic_factory = MosaicTilerFactory(
     layer_dependency=AssetsBidxExprParams,
     router_prefix=get_settings().mosaic_endpoint_prefix,
 )
+
+
+@pgstac_mosaic_factory.router.get(
+    "/info", response_model=MosaicInfo, response_class=ORJSONResponse
+)
+def map(
+    request: Request, collection: str = Query(..., description="STAC Collection ID")
+) -> ORJSONResponse:
+    collection_config = get_collection_config(collection)
+    if not collection_config or not collection_config.mosaic_info:
+        return ORJSONResponse(
+            status_code=404,
+            content=f"No mosaic info available for collection {collection}",
+        )
+
+    return ORJSONResponse(
+        status_code=200,
+        content=collection_config.mosaic_info.dict(by_alias=True, exclude_unset=True),
+    )
---
+++ pctiler/tests/endpoints/test_pg_mosaic.py
@@ -0,0 +1,10 @@
+from fastapi.testclient import TestClient
+from pccommon.config.collections import MosaicInfo
+
+
+def test_get(client: TestClient) -> None:
+    response = client.get("/mosaic/info?collection=naip")
+    assert response.status_code == 200
+    info_dict = response.json()
+    mosaic_info = MosaicInfo(**info_dict)
+    assert mosaic_info.default_location.zoom == 13
