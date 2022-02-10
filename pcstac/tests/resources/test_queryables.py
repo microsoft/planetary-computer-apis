06c6c46903d1e9234dead2545fe07dc7186abf71
Rob Emanuele <rdemanuele@gmail.com>
2022-02-09 21:10:50 -0500

    Fetch queryables from storage tables

--- pcstac/tests/resources/test_queryables.py
+++ pcstac/tests/resources/test_queryables.py
@@ -1,10 +1,8 @@
-from typing import Callable
-
 import pytest
 
 
 @pytest.mark.asyncio
-async def test_queryables(app_client, load_test_data: Callable):
+async def test_queryables(app_client):
     resp = await app_client.get("/queryables")
     assert resp.status_code == 200
     properties = resp.json()["properties"]
@@ -15,7 +13,28 @@ async def test_queryables(app_client, load_test_data: Callable):
 
 
 @pytest.mark.asyncio
-async def test_collection_queryables(app_client, load_test_data: Callable):
+async def test_queryables_io_lulc(app_client):
+    resp = await app_client.get("/queryables")
+    assert resp.status_code == 200
+    properties = resp.json()["properties"]
+    assert "id" in properties
+    assert "datetime" in properties
+    assert "naip:year" in properties
+    assert "naip:state" in properties
+
+
+@pytest.mark.asyncio
+async def test_collection_queryables_io_lulc(app_client):
+    resp = await app_client.get("/collections/io-lulc/queryables")
+    assert resp.status_code == 200
+    properties = resp.json()["properties"]
+    assert "id" in properties
+    assert "datetime" in properties
+    assert "io:supercell_id" in properties
+
+
+@pytest.mark.asyncio
+async def test_collection_queryables_naip(app_client):
     resp = await app_client.get("/collections/naip/queryables")
     assert resp.status_code == 200
     properties = resp.json()["properties"]
@@ -26,6 +45,6 @@ async def test_collection_queryables(app_client, load_test_data: Callable):
 
 
 @pytest.mark.asyncio
-async def test_collection_queryables_404(app_client, load_test_data: Callable):
+async def test_collection_queryables_404(app_client):
     resp = await app_client.get("/collections/does-not-exist/queryables")
     assert resp.status_code == 404
