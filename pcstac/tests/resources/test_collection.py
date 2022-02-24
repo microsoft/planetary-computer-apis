f746e209906f9b1d16ae1198c49c1e723096014d
Rob Emanuele <rdemanuele@gmail.com>
2022-02-24 02:18:46 -0500

    Add test for rate limit, but skip as it is nondeterministic

--- pcstac/tests/resources/test_collection.py
+++ pcstac/tests/resources/test_collection.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 
@@ -10,7 +12,7 @@ async def test_get_collections(app_client):
 
 @pytest.mark.asyncio
 async def test_get_collection(app_client):
-    """Test read a collection which does not exist"""
+    """Test read a collection which does exist"""
     resp = await app_client.get("/collections/naip")
     assert resp.status_code == 200
 
@@ -19,4 +21,5 @@ async def test_get_collection(app_client):
 async def test_collection_not_found(app_client):
     """Test read a collection which does not exist"""
     resp = await app_client.get("/collections/does-not-exist")
+    print(json.dumps(resp.json(), indent=2))
     assert resp.status_code == 404
---
+++ pcstac/tests/test_rate_limit.py
@@ -0,0 +1,17 @@
+import pytest
+
+from pccommon.constants import HTTP_429_TOO_MANY_REQUESTS
+
+
+@pytest.mark.asyncio
+async def test_rate_limit_collection(app_client):
+    pytest.skip("Non-deterministic. Set the rate limit in the config file to test.")
+    resp = None
+    for _ in range(0, 1001):
+        resp = await app_client.get("/collections/naip")
+        if resp.status_code == HTTP_429_TOO_MANY_REQUESTS:
+            break
+        else:
+            assert resp.status_code == 200
+
+    assert resp.status_code == HTTP_429_TOO_MANY_REQUESTS
