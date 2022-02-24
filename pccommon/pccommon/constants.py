37a7c7bf3bc9108e4485949f407951a0fb13743f
Rob Emanuele <rdemanuele@gmail.com>
2022-02-24 01:34:10 -0500

    Refactor get_request_ip into utils

--- pccommon/pccommon/constants.py
+++ pccommon/pccommon/constants.py
@@ -2,3 +2,8 @@ DEFAULT_COLLECTION_CONFIG_TABLE_NAME = "collectionconfig"
 DEFAULT_CONTAINER_CONFIG_TABLE_NAME = "containerconfig"
 
 DEFAULT_TABLE_TTL = 600  # 10 minutes
+# Headers containing information about the requester's
+# IP address. Checked in the order listed here.
+X_AZURE_CLIENTIP = "X-Azure-ClientIP"
+X_ORIGINAL_FORWARDED_FOR = "X-Original-Forwarded-For"
+X_FORWARDED_FOR = "X-Forwarded-For"
