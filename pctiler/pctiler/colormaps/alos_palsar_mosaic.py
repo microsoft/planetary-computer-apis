06f486212e34dd2a7145d8f0148c26f04870e55c
Tom Augspurger <taugspurger@microsoft.com>
2022-03-31 20:39:26 -0500

    lint

--- pctiler/pctiler/colormaps/alos_palsar_mosaic.py
+++ pctiler/pctiler/colormaps/alos_palsar_mosaic.py
@@ -12,8 +12,8 @@ alos_palsar_mosaic_colormaps: Dict[str, ColorMapType] = {
         255: (168, 153, 135, 255),
     },
     "alos-fnf": {
-        1: (57, 125, 73, 255),    # forest
+        1: (57, 125, 73, 255),  # forest
         2: (227, 226, 195, 255),  # non-forest
-        3: (65, 155, 223, 255),   # water
-    }
+        3: (65, 155, 223, 255),  # water
+    },
 }
