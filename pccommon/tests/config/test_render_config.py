99051cffc3c5224191802e04ff5c64f2e78b53ba
Tom Augspurger <taugspurger@microsoft.com>
2022-05-05 13:55:29 -0500

    Updated test

--- pccommon/tests/config/test_render_config.py
+++ pccommon/tests/config/test_render_config.py
@@ -5,13 +5,13 @@ from pccommon.config.collections import DefaultRenderConfig
 
 multi_assets = DefaultRenderConfig(
     assets=["data1", "data2"],
-    render_params={"colormap_name": "terrain", "rescale": [-1000, 4000]},
+    render_params={"colormap_name": "terrain", "rescale": "-1000,4000"},
     minzoom=8,
 )
 
 single_asset = DefaultRenderConfig(
     assets=["data1"],
-    render_params={"colormap_name": "terrain", "rescale": [-1000, 4000]},
+    render_params={"colormap_name": "terrain", "rescale": "-1000,4000"},
     minzoom=8,
 )
 
@@ -29,7 +29,7 @@ def test_multi_asset() -> None:
     qs = multi_assets.get_full_render_qs("my_collection_id", "my_item_id")
     assert qs == (
         "collection=my_collection_id&item=my_item_id&"
-        "assets=data1&assets=data2&colormap_name=terrain&rescale=-1000&rescale=4000"
+        "assets=data1&assets=data2&colormap_name=terrain&rescale=-1000%2C4000"
     )
 
 
@@ -37,7 +37,7 @@ def test_single_asset() -> None:
     qs = single_asset.get_full_render_qs("my_collection_id", "my_item_id")
     assert qs == (
         "collection=my_collection_id&item=my_item_id&assets=data1&"
-        "colormap_name=terrain&rescale=-1000&rescale=4000"
+        "colormap_name=terrain&rescale=-1000%2C4000"
     )
 
 
@@ -46,7 +46,7 @@ def test_no_asset() -> None:
     encoded_params = quote_plus("asset1,0.45*asset2,asset3/asset1")
     assert qs == (
         f"collection=my_collection_id&item=my_item_id&expression={encoded_params}"
-        "&colormap_name=terrain&rescale=-1000&rescale=4000"
+        "&colormap_name=terrain&rescale=-1000%2C4000"
     )
 
 
@@ -54,7 +54,7 @@ def test_collection_only() -> None:
     qs = single_asset.get_full_render_qs("my_collection_id")
     assert qs == (
         "collection=my_collection_id&assets=data1&colormap_name=terrain&"
-        "rescale=-1000&rescale=4000"
+        "rescale=-1000%2C4000"
     )
 
 
