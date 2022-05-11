b9b4cdf10a7d8d000c2d5d98cdf9531348f7db10
Matt McFarland <mmcfarland@microsoft.com>
2022-05-11 13:30:10 -0400

    Add PC_SDK_SUBSCRIPTION_KEY to tiler chart
    This allows the tiler to request SAS tokens through a PC account that
    has specialized permissions.

--- deployment/terraform/resources/keyvault.tf
+++ deployment/terraform/resources/keyvault.tf
@@ -7,3 +7,8 @@ data "azurerm_key_vault_secret" "db_admin_password" {
   name         = var.pg_password_secret_name
   key_vault_id = data.azurerm_key_vault.deploy_secrets.id
 }
+
+data "azurerm_key_vault_secret" "pc_sdk_subscription_key" {
+  name         = var.pc_sdk_subscription_key_secret_name
+  key_vault_id = data.azurerm_key_vault.deploy_secrets.id
+}
