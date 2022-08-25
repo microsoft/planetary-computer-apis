2b8c0ee151fe475ecbbc6220336f8569a86fc5ad
Tom Augspurger <taugspurger@microsoft.com>
2022-08-25 10:19:54 -0500

    azurerm 2.x compatability

--- deployment/terraform/resources/aks.tf
+++ deployment/terraform/resources/aks.tf
@@ -22,10 +22,19 @@ resource "azurerm_kubernetes_cluster" "pc" {
     type = "SystemAssigned"
   }
 
-  azure_active_directory_role_based_access_control {
-    managed = true
-    azure_rbac_enabled = true
+  role_based_access_control {
+    enabled = true
+    azure_active_directory {
+      managed = true
+      azure_rbac_enabled = true
+    }
+
   }
+  # TODO(azurerm 3.x)
+  # azure_active_directory_role_based_access_control {
+  #   managed = true
+  #   azure_rbac_enabled = true
+  # }
 
   tags = {
     Environment = var.environment
