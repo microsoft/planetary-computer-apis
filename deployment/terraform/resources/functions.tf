528259cc9cce33f3971586ed8848c4dbac092b87
Matt McFarland <mmcfarland@microsoft.com>
2022-09-19 10:15:54 -0400

    Setup local and deployed funcs to use CORS
    * nginx proxy facilitates CORS headers locally
    * azurite container set to public access to local frontend can access
    animation urls
    * configure func apps deployed with CORS enabled

--- deployment/terraform/resources/functions.tf
+++ deployment/terraform/resources/functions.tf
@@ -48,17 +48,21 @@ resource "azurerm_function_app" "pcfuncs" {
   site_config {
     linux_fx_version          = "PYTHON|3.8"
     use_32_bit_worker_process = false
+
+    cors {
+      allowed_origins = ["*"]
+    }
   }
 }
 
 # Note: this must be in the same subscription as the rest of the deployed infrastructure
 data "azurerm_storage_container" "output" {
-  name                = var.output_container_name
+  name                 = var.output_container_name
   storage_account_name = var.output_storage_account_name
 }
 
 resource "azurerm_role_assignment" "function-app-animation-container-access" {
-  scope = data.azurerm_storage_container.output.resource_manager_id
+  scope                = data.azurerm_storage_container.output.resource_manager_id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_function_app.pcfuncs.identity[0].principal_id
 
