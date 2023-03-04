aada1bd72e9e8afafbe915ac923f9e182d8a7ff6
Matt McFarland <mmcfarland@microsoft.com>
2023-03-03 20:15:14 -0500

    Upgrade AKS to supported version (#158)
    * Upgrades aks to supported version
    
    * Formatting

--- deployment/terraform/staging/main.tf
+++ deployment/terraform/staging/main.tf
@@ -2,25 +2,25 @@ module "resources" {
   source = "../resources"
 
   environment = "staging"
-  region = "West Europe"
+  region      = "West Europe"
 
-  k8s_version = "1.22.6"
+  k8s_version = "1.25.5"
 
   cluster_cert_issuer = "letsencrypt"
   cluster_cert_server = "https://acme-v02.api.letsencrypt.org/directory"
 
-  aks_node_count = 2
-  stac_replica_count = 2
+  aks_node_count      = 2
+  stac_replica_count  = 2
   tiler_replica_count = 2
 
   # Funcs
-  output_storage_account_name = "pcfilestest"
-  output_container_name = "output"
-  funcs_data_api_url = "https://planetarycomputer.microsoft.com/api/data/v1"
+  output_storage_account_name    = "pcfilestest"
+  output_container_name          = "output"
+  funcs_data_api_url             = "https://planetarycomputer.microsoft.com/api/data/v1"
   funcs_tile_request_concurrency = 10
 
   animation_output_storage_url = "https://pcfilestest.blob.core.windows.net/output/animations"
-  image_output_storage_url = "https://pcfilestest.blob.core.windows.net/output/images"
+  image_output_storage_url     = "https://pcfilestest.blob.core.windows.net/output/images"
 
 }
 
@@ -34,6 +34,6 @@ terraform {
 }
 
 output "resources" {
-  value = module.resources
+  value     = module.resources
   sensitive = true
 }
