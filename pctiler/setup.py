ffd71abdde384aedaaaf6a764a541ca514e64283
Matt McFarland <mmcfarland@microsoft.com>
2023-04-12 16:11:36 -0400

    Create slimmer image

--- pctiler/setup.py
+++ pctiler/setup.py
@@ -2,23 +2,8 @@
 
 from setuptools import find_packages, setup
 
-# Runtime requirements.
-inst_reqs = [
-    # "geojson-pydantic==0.4.2",
-    # "jinja2==3.0.3",
-    # "pystac==1.*",
-    # "planetary-computer==0.4.*",
-    # "rasterio==1.3.*",
-    # "titiler.core==0.10.2",
-    # "titiler.mosaic==0.10.2",
-    # # titiler-pgstac
-    # "psycopg[binary,pool]",
-    # "titiler.pgstac==0.2.2",
-    # # colormap dependencies
-    # "matplotlib==3.4.*",
-    # "importlib_resources>=1.1.0;python_version<'3.9'",
-    # "pccommon",
-]
+# Runtime requirements, see environment.yaml
+inst_reqs = []
 
 extra_reqs = {
     "server": [
