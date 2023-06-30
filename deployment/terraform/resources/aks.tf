070e62201d53e9cc30ab4396e5b2b80a5c1750f1
Martin Cappi Dahl <mcappi91@gmail.com>
2023-06-30 11:24:21 -0300

    revert aks network plugin

--- deployment/terraform/resources/aks.tf
+++ deployment/terraform/resources/aks.tf
@@ -17,8 +17,7 @@ resource "azurerm_kubernetes_cluster" "pc" {
   }
 
   network_profile {
-    network_plugin    = "azure"
-    network_policy    = "azure"
+    network_plugin    = "kubenet"
     load_balancer_sku = "standard"
   }
 
