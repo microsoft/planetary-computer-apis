95bfabd873ddfee177ee40029d5ebc67f5d98436
elayrocks <kinghyl13@gmail.com>
2024-05-01 23:48:57 -0700

    test

--- deployment/terraform/staging/main.tf
+++ deployment/terraform/staging/main.tf
@@ -32,7 +32,6 @@ terraform {
     storage_account_name = "pctesttfstate"
     container_name       = "pc-test-api"
     key                  = "pqe-apis.tfstate"
-    use_oidc             = true
   }
 }
 
