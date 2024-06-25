9b8282df0b2e377d84ce415ac595bf963addc0be
elay <yileihu@microsoft.com>
2024-06-24 17:08:29 -0700

    UPDATE function settings

--- pcfuncs/ipban/config.py
+++ pcfuncs/ipban/config.py
@@ -1,18 +1,17 @@
 # config.py
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    # Constants related to Azure Table Storage
-    storage_account_url: str = "https://pctapisstagingsa.table.core.windows.net/"
-    banned_ip_table: str = "blobstoragebannedip"
-
-    # Log Analytics Workspace: pc-api-loganalytics
-    log_analytics_workspace_id: str = "78d48390-b6bb-49a9-b7fd-a86f6522e9c4"
+    storage_account_url: str = Field(env="STORAGE_ACCOUNT_URL")
+    banned_ip_table: str = Field(env="BANNED_IP_TABLE")
+    log_analytics_workspace_id: str = Field(env="LOG_ANALYTICS_WORKSPACE_ID")
 
     # Time and threshold settings
-    time_window_in_hours: int = 24
-    threshold_read_count_in_gb: int = 5120
+    time_window_in_hours: int = Field(default=24, env="TIME_WINDOW_IN_HOURS")
+    threshold_read_count_in_gb: int = Field(
+        default=5120, env="THRESHOLD_READ_COUNT_IN_GB"
+    )
 
 
 # Create a global settings instance
