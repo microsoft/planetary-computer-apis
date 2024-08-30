51ca4e8cf84aae2d7e25823bac9dc8c3f5e8cd5e
Marc Lichtman <marcll@vt.edu>
2024-08-30 13:52:28 -0400

    Fixed searchid bug, added unit tests, improved main readme (#236)
    * fixed searchid bug and added unit tests
    
    * readme tweaks
    
    * black line length
    
    * change line length in flake settings
    
    * revert line length change, going to do it in separate PR
    
    * autoformatter
    
    * retrigger github actions
    
    * Delete scripts/test-mosaic
    
    * hack for not needing secrets AZURITE_ACCOUNT_KEY
    
    * shorten command
    
    * testing output
    
    * remove grep
    
    * quotes
    
    * trying another method
    
    * trying another method
    
    * remove extra echo
    
    * tweaks
    
    * remove echo
    
    * use env
    
    * test failure
    
    * fail step
    
    * try removing azurite from api validator
    
    * autoformatter
    
    * pipeline tweak
    
    * Delete storage-use-azurite?tabs=visual-studio,blob-storage
    
    ---------
    
    Co-authored-by: Marc Lichtman <marclichtman@microsoft.com>

--- pctiler/tests/endpoints/test_pg_mosaic.py
+++ pctiler/tests/endpoints/test_pg_mosaic.py
@@ -10,7 +10,6 @@ REGISTER_TYPE = Tuple[str, Dict[str, Any]]
 
 @pytest.fixture
 async def register_search(client: AsyncClient) -> REGISTER_TYPE:
-
     cql = {
         "filter-lang": "cql2-json",
         "filter": {
@@ -18,11 +17,38 @@ async def register_search(client: AsyncClient) -> REGISTER_TYPE:
             "args": [{"op": "=", "args": [{"property": "collection"}, "naip"]}],
         },
     }
-    expected_content_hash = "8b989f86a149628eabfde894fb965982"
     response = await client.post("/mosaic/register", json=cql)
+    expected_content_hash = response.json()["searchid"]
     resp = response.json()
-
     return (expected_content_hash, resp)
+
+
+async def test_register_search_with_geom(client: AsyncClient) -> None:
+    geom = {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [-79.09062791441062, 43.08554661560049],
+                [-79.0629876337021, 43.08554661560049],
+                [-79.0629876337021, 43.067969831431895],
+                [-79.09062791441062, 43.067969831431895],
+                [-79.09062791441062, 43.08554661560049],
+            ]
+        ],
+    }
+    cql = {
+        "filter-lang": "cql2-json",
+        "filter": {
+            "op": "and",
+            "args": [
+                {"op": "=", "args": [{"property": "collection"}, "naip"]},
+                {"op": "s_intersects", "args": [{"property": "geometry"}, geom]},
+            ],
+        },
+    }
+    response = await client.post("/mosaic/register", json=cql)
+    assert response.status_code == 200
+    assert response.json()["searchid"] == "2607eab51afd5d626da8d50d9df3bbf0"
 
 
 @pytest.mark.asyncio
@@ -36,18 +62,20 @@ async def test_mosaic_info(client: AsyncClient) -> None:
 
 @pytest.mark.asyncio
 async def test_register(client: AsyncClient, register_search: REGISTER_TYPE) -> None:
-
     expected_content_hash, register_resp = register_search
 
     # Test that `searchid` which has been removed in titiler remains in pctiler,
     # and that the search hash remains consistent
     assert register_resp["searchid"] == expected_content_hash
     # Test that the links have had the {tileMatrixSetId} template string removed
-    assert len(register_resp["links"]) == 2
+    assert len(register_resp["links"]) == 3
     assert register_resp["links"][0]["href"].endswith(
+        f"/mosaic/{expected_content_hash}/info"
+    )  # gets added by searchInfoExtension
+    assert register_resp["links"][1]["href"].endswith(
         f"/mosaic/{expected_content_hash}/tilejson.json"
     )
-    assert register_resp["links"][1]["href"].endswith(
+    assert register_resp["links"][2]["href"].endswith(
         f"/mosaic/{expected_content_hash}/WMTSCapabilities.xml"
     )
 
@@ -99,6 +127,51 @@ async def test_mosaic_tile_routes(
     )
     response = await client.get(url)
     assert response.status_code == 200
+
+
+async def test_info_path_with_searchid(
+    client: AsyncClient, register_search: REGISTER_TYPE
+) -> None:
+    # route source code is in titiler/mosaic/titiler/mosaic/factory.py#L157
+    # the searchId functionality is added by titiler-pgstac
+    route = "mosaic/{searchId}/info"
+    expected_content_hash, _ = register_search
+    formatted_route = route.format(searchId=expected_content_hash)
+    url = (
+        f"/{formatted_route}?asset_bidx=image%7C1%2C2%2C3&assets=image&collection=naip"
+    )
+    response = await client.get(url)
+    assert response.status_code == 200
+
+
+async def test_info_path_with_bad_searchid(client: AsyncClient) -> None:
+    route = "mosaic/{searchId}/info"
+
+    # does not match the one we registered in the fixture
+    expected_content_hash = "9b989f86a149628eabfde894fb965982"
+
+    formatted_route = route.format(searchId=expected_content_hash)
+    url = (
+        f"/{formatted_route}?asset_bidx=image%7C1%2C2%2C3&assets=image&collection=naip"
+    )
+    response = await client.get(url)
+    assert response.status_code == 404
+
+
+async def test_bad_searchid(client: AsyncClient) -> None:
+    route = "mosaic/tiles/{searchId}/{z}/{x}/{y}"
+
+    # does not match the one we registered in the fixture
+    expected_content_hash = "9b989f86a149628eabfde894fb965982"
+
+    formatted_route = route.format(
+        searchId=expected_content_hash, z=16, x=17218, y=26838
+    )
+    url = (
+        f"/{formatted_route}?asset_bidx=image%7C1%2C2%2C3&assets=image&collection=naip"
+    )
+    response = await client.get(url)
+    assert response.status_code == 404
 
 
 @pytest.mark.asyncio
