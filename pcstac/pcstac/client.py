18557cc7a62708d6c7f7854efde868a2c5b3f142
vincentsarago <vincent.sarago@gmail.com>
2026-01-16 18:47:11 +0100

    avoid calling all_collections on landing page

--- pcstac/pcstac/client.py
+++ pcstac/pcstac/client.py
@@ -8,6 +8,7 @@ import orjson
 from fastapi import HTTPException, Request
 from stac_fastapi.pgstac.core import CoreCrudClient
 from stac_fastapi.types.errors import NotFoundError
+from stac_fastapi.types.requests import get_base_url
 from stac_fastapi.types.stac import (
     Collection,
     Collections,
@@ -15,6 +16,8 @@ from stac_fastapi.types.stac import (
     ItemCollection,
     LandingPage,
 )
+from stac_pydantic.links import Relations
+from stac_pydantic.shared import MimeTypes
 
 from pccommon.config import get_all_render_configs, get_render_config
 from pccommon.config.collections import DefaultRenderConfig
@@ -238,11 +241,87 @@ class PCClient(CoreCrudClient):
         return await cached_result(_fetch, cache_key, request)
 
     async def landing_page(self, request: Request, **kwargs: Any) -> LandingPage:
-        _super: CoreCrudClient = super()
+        """Landing page."""
 
         async def _fetch() -> LandingPage:
-            landing = await _super.landing_page(request=request, **kwargs)
-            return landing
+            """Landing page.
+
+            NOTE: we need a custom landing page implementation
+            to avoid the call to `all_collections` method.
+
+            TODO: replace this with:
+            ```
+            _super: CoreCrudClient = super()
+
+            async def _fetch() -> LandingPage:
+                landing = await _super.landing_page(request=request, **kwargs)
+                return landing
+            ```
+            when switching to stac-fastapi >=v5.1.
+
+            """
+            base_url = get_base_url(request)
+
+            landing_page = self._landing_page(
+                base_url=base_url,
+                conformance_classes=self.conformance_classes(),
+                extension_schemas=[],
+            )
+
+            # Add Queryables link
+            if self.extension_is_enabled(
+                "FilterExtension"
+            ) or self.extension_is_enabled("SearchFilterExtension"):
+                landing_page["links"].append(
+                    {
+                        "rel": Relations.queryables.value,
+                        "type": MimeTypes.jsonschema.value,
+                        "title": "Queryables available for this Catalog",
+                        "href": urljoin(base_url, "queryables"),
+                        "method": "GET",
+                    }
+                )
+
+            # Add Aggregation links
+            if self.extension_is_enabled("AggregationExtension"):
+                landing_page["links"].extend(
+                    [
+                        {
+                            "rel": "aggregate",
+                            "type": "application/json",
+                            "title": "Aggregate",
+                            "href": urljoin(base_url, "aggregate"),
+                        },
+                        {
+                            "rel": "aggregations",
+                            "type": "application/json",
+                            "title": "Aggregations",
+                            "href": urljoin(base_url, "aggregations"),
+                        },
+                    ]
+                )
+
+            # Add OpenAPI URL
+            landing_page["links"].append(
+                {
+                    "rel": Relations.service_desc.value,
+                    "type": MimeTypes.openapi.value,
+                    "title": "OpenAPI service description",
+                    "href": str(request.url_for("openapi")),
+                }
+            )
+
+            # Add human readable service-doc
+            landing_page["links"].append(
+                {
+                    "rel": Relations.service_doc.value,
+                    "type": MimeTypes.html.value,
+                    "title": "OpenAPI service documentation",
+                    "href": str(request.url_for("swagger_ui_html")),
+                }
+            )
+
+            return LandingPage(**landing_page)
 
         return await cached_result(_fetch, CACHE_KEY_LANDING_PAGE, request)
 
